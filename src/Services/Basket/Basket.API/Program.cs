bc9d62d56396cf5974eb3e8d977b276cba60e913
Saim Mamoune Abdellah <abdellah.saimmamoune1@gmail.com>
2026-01-29 16:34:53 +0100

    adding resdis,controller,repo

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using Basket.API.Repositories;
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+//redis
+builder.Services.AddSingleton<IConnectionMultiplexer>(sp =>
+{
+    var connectionString = builder.Configuration.GetValue<string>("Redis:ConnectionString");
+
+    if (string.IsNullOrEmpty(connectionString))
+        throw new InvalidOperationException("Redis connection string is not configured");
+
+    return ConnectionMultiplexer.Connect(connectionString);
+});
+
+
+builder.Services.AddScoped<IDatabase>(sp =>
+{
+    var multiplexer = sp.GetRequiredService<IConnectionMultiplexer>();
+    return multiplexer.GetDatabase();
+});
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 
 var app = builder.Build();
 
